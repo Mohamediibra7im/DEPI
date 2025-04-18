b1ab2bec06ae914584864dec860e422b506a727c
Hanan-Hany <hananhanyfathy@gmail.com>
2025-04-18 07:59:25 +0200

    Add Student Model

---
+++ CMS/CMS/Migrations/20250418055609_AddStudentTableToDb.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CMS.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStudentTableToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NationalId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BirthDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    EnrollmentDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GPA = table.Column<decimal>(type: "decimal(3,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
---
+++ CMS/CMS/Models/Students.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CMS.Models
+{
+    public class Students 
+    {
+        public int Id { get; set; }                      // Auto Genrated
+        public string IdNumber { get; set; }             // Custom ID (e.g., "20230001")  year + 4digit Last one is Id
+        public string NationalId { get; set; }     
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email => $"{IdNumber}@college.edu.eg".ToLower();
+        public DateOnly BirthDate { get; set; }
+        public DateOnly EnrollmentDate { get; set; }
+        public string PhoneNumber {  get; set; }
+        public string Address { get; set; }
+        [Column(TypeName = "decimal(3,2)")]
+        public decimal GPA { get; set; }
+
+
+
+    }
+}
