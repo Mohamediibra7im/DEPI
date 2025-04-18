379c94ed1053a39100d2c08ecd745cf6c1bf0888
Hanan-Hany <hananhanyfathy@gmail.com>
2025-04-18 07:57:49 +0200

    Initial DbContext

---
+++ CMS/CMS/Data/ApplicaionDbContext.cs
@@ -0,0 +1,13 @@
+﻿using CMS.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CMS.Data
+{
+    public class ApplicaionDbContext : DbContext
+    {
+        public ApplicaionDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Students> Students { get; set; }
+    }
+}
