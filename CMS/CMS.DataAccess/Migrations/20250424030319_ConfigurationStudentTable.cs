decf4e3b8deaa6dce4362c8bdcb6ee89dcca5e44
Hanan-Hany <hananhanyfathy@gmail.com>
2025-04-24 06:03:09 +0200

    Add Student Configuration

---
+++ CMS/CMS.DataAccess/Migrations/20250424030319_ConfigurationStudentTable.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CMS.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConfigurationStudentTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Students",
+                type: "nvarchar(11)",
+                maxLength: 11,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NationalId",
+                table: "Students",
+                type: "nvarchar(14)",
+                maxLength: 14,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "GPA",
+                table: "Students",
+                type: "decimal(3,2)",
+                nullable: false,
+                defaultValue: 4.0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(3,2)");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "EnrollmentDate",
+                table: "Students",
+                type: "date",
+                nullable: false,
+                defaultValueSql: "GETDATE()",
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(11)",
+                oldMaxLength: 11);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NationalId",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(14)",
+                oldMaxLength: 14);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "GPA",
+                table: "Students",
+                type: "decimal(3,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(3,2)",
+                oldDefaultValue: 4.0m);
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "EnrollmentDate",
+                table: "Students",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date",
+                oldDefaultValueSql: "GETDATE()");
+        }
+    }
+}
