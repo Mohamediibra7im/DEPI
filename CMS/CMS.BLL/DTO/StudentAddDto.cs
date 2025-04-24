967bf5919a600b70cdd104d10cc3d52aee9bd605
Hanan-Hany <hananhanyfathy@gmail.com>
2025-04-24 06:03:41 +0200

    Add Student DTo

---
+++ CMS/CMS.BLL/DTO/StudentAddDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.BLL.DTO
+{
+    public class StudentAddDto
+    {
+        [Required]
+        [RegularExpression(@"^\d{14}$", ErrorMessage = " National Id must contain only digits and be 14 characters long.")]
+        [StringLength(14, MinimumLength = 14)]
+        public string NationalId { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public DateOnly BirthDate { get; set; }
+        [Required]
+        [RegularExpression(@"^\d{11}$", ErrorMessage = "Phone number must contain only digits and be 11 characters long.")]
+        [StringLength(11, MinimumLength = 11)]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string Address { get; set; }
+
+    }
+}
