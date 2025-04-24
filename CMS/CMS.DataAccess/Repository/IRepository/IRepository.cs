e9dc9c197dd0c2b74da2fcb8b25336007abddf54
Hanan-Hany <hananhanyfathy@gmail.com>
2025-04-24 06:00:45 +0200

    Add Repository

---
+++ CMS/CMS.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        //T Get(Expression<Func<T, bool>> filter);
+
+        T GetById(int id);
+        void Add(T item);
+
+        void Remove(T item);
+        void Update(T item); 
+
+    }
+}
---
+++ CMS/CMS.DataAccess/Repository/Repository.cs
@@ -0,0 +1,56 @@
+﻿using CMS.Data;
+using CMS.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicaionDbContext _db;
+        internal DbSet<T> dbSet;
+        public Repository(ApplicaionDbContext db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>();
+        }
+        public void Add(T item)
+        {
+            dbSet.Add(item);
+        }
+
+        //public T Get(Expression<Func<T, bool>> filter)
+        //{
+        //IQueryable<T> query = dbSet.Where(filter).AsNoTracking();
+
+        //return query.FirstOrDefault();
+        //}
+
+        public T GetById(int id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbSet;
+
+            return query.ToList();
+        }
+
+        public void Remove(T item)
+        {
+            dbSet.Remove(item);
+        }
+
+        public void Update(T item)
+        {
+           dbSet.Update(item);
+        }
+    }
+}
