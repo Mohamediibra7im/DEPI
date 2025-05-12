576ed9161ea8427f43fd1e1c423e839de3f35158
Hanan-Hany <hananhanyfathy@gmail.com>
2025-05-13 02:24:01 +0300

    Add Tables

---
+++ CMS/CMS.DataAccess/Migrations/20250512173916_AddCorrectOption.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CMS.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCorrectOption : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CorrectOptionId",
+                table: "ExamQuestions");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Marks",
+                table: "ExamQuestions",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(5,2)");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCorrect",
+                table: "ExamOption",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsCorrect",
+                table: "ExamOption");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Marks",
+                table: "ExamQuestions",
+                type: "decimal(5,2)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CorrectOptionId",
+                table: "ExamQuestions",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
