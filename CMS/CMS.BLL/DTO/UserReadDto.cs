8f29a87c1da4c7a47f3c485840580f3acd967c6e
Hanan-Hany <hananhanyfathy@gmail.com>
2025-05-13 02:31:10 +0300

    Add DTOs for user creation, reading, and updating

---
+++ CMS/CMS.BLL/DTO/UserReadDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.BLL.DTO
+{
+    public class UserReadDto
+    {
+        public int Id { get; set; }                      
+        public string IdNumber { get; set; }
+        public string FullName { get; set; }
+        
+        [NotMapped]
+        public string Email => $"{IdNumber}@college.edu.eg".ToLower();
+
+        public decimal GPA { get; set; }
+    }
+}
