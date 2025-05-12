7c24bdf0d8afd37494ac7c21df74b5bf887b4748
Hanan-Hany <hananhanyfathy@gmail.com>
2025-05-13 01:53:07 +0300

    Add repository for managing ExamQuestions entities

---
+++ CMS/CMS.DataAccess/Repository/IRepository/IQuestionExamRepository.cs
@@ -0,0 +1,18 @@
+﻿using CMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.DataAccess.Repository.IRepository
+{
+    public interface IQuestionExamRepository:IRepository<ExamQuestions>
+    {
+        IEnumerable<ExamQuestions> GetAll(int examid);
+
+
+        void Save();
+    }
+    
+}
---
+++ CMS/CMS.DataAccess/Repository/QuestionExamRepository.cs
@@ -0,0 +1,37 @@
+﻿using CMS.Data;
+using CMS.DataAccess.Models;
+using CMS.DataAccess.Repository.IRepository;
+using CMS.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.DataAccess.Repository
+{
+    public class QuestionExamRepository : Repository<ExamQuestions>, IQuestionExamRepository
+    {
+        private readonly ApplicaionDbContext _db;
+        public QuestionExamRepository(ApplicaionDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<ExamQuestions> GetAll(int examsid)
+        {
+            IQueryable<ExamQuestions> query = dbSet
+               .Include(q => q.Options)
+               .Where(q => q.ExamId == examsid);
+
+            return query.ToList();
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+    
+}
