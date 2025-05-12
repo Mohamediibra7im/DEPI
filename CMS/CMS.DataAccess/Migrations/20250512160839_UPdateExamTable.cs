9a5008be6b9dbc7fdd89a604659b634d7a6c9447
Hanan-Hany <hananhanyfathy@gmail.com>
2025-05-13 02:10:55 +0300

    Update Exam Table

---
+++ CMS/CMS.DataAccess/Migrations/20250512160839_UPdateExamTable.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CMS.Migrations
+{
+    /// <inheritdoc />
+    public partial class UPdateExamTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ExamQuestions_Exams_ExamsId",
+                table: "ExamQuestions");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ExamsId",
+                table: "ExamQuestions",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ExamId",
+                table: "ExamQuestions",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ExamQuestions_Exams_ExamsId",
+                table: "ExamQuestions",
+                column: "ExamsId",
+                principalTable: "Exams",
+                principalColumn: "ExamsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ExamQuestions_Exams_ExamsId",
+                table: "ExamQuestions");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ExamsId",
+                table: "ExamQuestions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ExamId",
+                table: "ExamQuestions",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ExamQuestions_Exams_ExamsId",
+                table: "ExamQuestions",
+                column: "ExamsId",
+                principalTable: "Exams",
+                principalColumn: "ExamsId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
