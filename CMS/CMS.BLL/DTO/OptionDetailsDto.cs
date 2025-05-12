2447800b380e81a40dbba6ad277969340c020a01
Hanan-Hany <hananhanyfathy@gmail.com>
2025-05-13 02:28:59 +0300

    Question DTOs

---
+++ CMS/CMS.BLL/DTO/OptionDetailsDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CMS.BLL.DTO
+{
+    public class OptionDetailsDto
+    {
+        public int OptionId { get; set; }
+        public string OptionText { get; set; }
+        public bool IsCorrect { get; set; }
+    }
+}
---
+++ CMS/CMS.BLL/DTO/QuestionDetailsDto.cs
@@ -0,0 +1,16 @@
+﻿using CMS.DataAccess.Models;
+using CMS.Models;
+using System.Collections.Generic;
+
+namespace CMS.BLL.DTO
+{
+    public class QuestionDetailsDto
+    {
+        public int ExamQuestionsId { get; set; }
+        public string QuestionText { get; set; }
+        public QuestionType QuestionType { get; set; }
+        public int Marks { get; set; }
+        public int ExamId { get; set; }
+        public List<OptionDetailsDto> Options { get; set; } = new();
+    }
+}
