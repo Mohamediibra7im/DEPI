3bd8968e587d3d59c3b2095d82915bc50943ab2f
Hanan-Hany <hananhanyfathy@gmail.com>
2025-05-13 02:06:05 +0300

    Add CourseRepository and ICourseRepository

---
+++ CMS/CMS.DataAccess/Repository/CourseRepository.cs
@@ -0,0 +1,28 @@
+﻿using CMS.Data;
+using CMS.DataAccess.Models;
+using CMS.DataAccess.Repository.IRepository;
+using CMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.DataAccess.Repository
+{
+    public class CourseRepository : Repository<Courses>, ICourseRepository
+    {
+        private readonly ApplicaionDbContext _db;
+        public CourseRepository(ApplicaionDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+
+    
+}
---
+++ CMS/CMS.DataAccess/Repository/IRepository/ICourseRepository.cs
@@ -0,0 +1,19 @@
+﻿using CMS.DataAccess.Models;
+using CMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.DataAccess.Repository.IRepository
+{
+    public interface ICourseRepository:IRepository<Courses>
+    {
+      
+        void Save();
+    }
+    
+     
+    
+}
