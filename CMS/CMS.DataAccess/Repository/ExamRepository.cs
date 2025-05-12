c5b42959b83cb9f813134a9f79c4a6618981e4c6
Hanan-Hany <hananhanyfathy@gmail.com>
2025-05-13 01:49:42 +0300

    Add ExamRepository and IExamRepository for exam management

---
+++ CMS/CMS.DataAccess/Repository/ExamRepository.cs
@@ -0,0 +1,27 @@
+﻿using CMS.Data;
+using CMS.DataAccess.Models;
+using CMS.DataAccess.Repository.IRepository;
+using CMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.DataAccess.Repository
+{
+    public class ExamRepository : Repository<Exams>, IExamRepository 
+    {
+        private readonly ApplicaionDbContext _db;
+        public ExamRepository(ApplicaionDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+    
+}
