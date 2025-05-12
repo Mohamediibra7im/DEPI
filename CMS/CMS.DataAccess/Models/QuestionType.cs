1bff72a53d0f7b4c23ce6185ac721b0dbf21638f
Hanan-Hany <hananhanyfathy@gmail.com>
2025-05-13 01:53:32 +0300

    Add Enum QuestionType

---
+++ CMS/CMS.DataAccess/Models/QuestionType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.DataAccess.Models
+{
+    public enum QuestionType
+    {
+        MCQ,        // Multiple Choice Question
+        TrueFalse,  // True or False
+        Essay       // Descriptive answer
+    }
+}
